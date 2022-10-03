376eaaa613a9d5301fc45f793366c04ca7b52bdd
smichaelonline <stephaniemichael001@gmail.com>
2022-10-03 09:42:56 -0400

    add entries router

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ import'./config/passport.js'
 // import routes
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
+import { router as entriesRouter  } from './routes/entries.js'
 
 // create the express app
 const app = express()
@@ -75,6 +76,7 @@ app.use(passDataToView)
 // router middleware
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
+app.use('/entries', entriesRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
